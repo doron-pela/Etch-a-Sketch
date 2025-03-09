1aca3d43a8d3475609a04b9828673f9ca8f8cba1
Doron Pela <devronpela@gmail.com>
2025-03-09 09:06:36 +0000

    Made grid dynamic and added a  hover trail

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+let size = 16;
+let input;
 
-const container = document.querySelector('.container');
+const button = document.querySelector('button');
+button.addEventListener('click', (e)=>{
+    input = parseInt(prompt('How many boxes big do you want your sketchpad to be?'));
+    size = input || size;
 
-for (let i = 0; i<16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    
-    container.appendChild(row);
+    const grid = document.querySelector('.grid');
+    grid.remove();
+    createGrid(size);
+});
 
-    for (let j = 0; j<16; j++){
-        const div = document.createElement('div');
-        div.classList.add('.box')
-        row.appendChild(div);
+createGrid(size);
+
+function createGrid(size){
+        
+    const container = document.querySelector('.container');
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let i = 0; i<size; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+
+        for (let j = 0; j<size; j++){
+            const div = document.createElement('div');
+            div.classList.add('box')
+            row.appendChild(div);
+        }
+
+        grid.appendChild(row);
     }
 
-    container.appendChild(row);
+
+    container.addEventListener('mouseover', (e)=>{
+        let div = e.target;
+        if(div.matches('.box')){
+            div.classList.add('hover');
+        }
+    });
+
+    container.appendChild(grid);
 }
-
-
